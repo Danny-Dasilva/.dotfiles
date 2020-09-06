c0f1973b2827ad67ba28fc94f95cbf2f91889329
Danny Dasilva <yahchayildasilva@gmail.com>
2020-09-06 19:20:47 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt install neovim
+sudo apt-get install xsel
+
+pip3 install pynvim
+#plugins
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+mkdir ~/.config/nvim/vim-plug
+
+touch ~/.config/nvim/vim-plug/plugins.vim
+
---
+++ test.py
@@ -0,0 +1,51 @@
+import socket
+import os
+lst = ['example', 'test']
+for count, i in enumerate(lst):
+    print(i, count)
+# example line
+ # gg and shift g to go up and down
+ # 0 first line and shift g for end of the lin
+ # line number shift g for snap
+ # x for delete and u for undo in normal mode crtl r for redo
+ # shift a for end of the line insert mode
+ # S deletes the letter and capital s the line
+ # o is for insert mode on a new line
+ # dw for delete word dd for delete line P to place line in location
+ # w for first word and e for end of word
+ # b for backward
+ # f forward till character / Capital is one before
+ # same for t but backwards until the character
+# yw for yank word /yy yanks an entire line
+# place them with put
+# percent to find the opening and closing parenthases
+# / for search in document use n and shift n for next and backwards
+# ctrl O for full/ since last file save revert control I for revert
+#search and replace ":  s/oldtring/newstring/g" % tack for whole document, or line#, line#
+# tack gc instead of g to ask for each instance
+# run command with :!command ex :!pwd
+#:W ctrl d for enumerating available commands
+# CRTL W in insert for delete word
+# caw or siw for the same
+
+
+# split commands
+#:split and vsplit / sp and vs
+# ctrl w then c to close split window
+# :10sp .bashrc
+# contrl w _ for minimize and = for equal
+# ctrl ww for swap windows
+# | for minimal split
+# swapping splits
+#:w capital R
+# I remapped splits
+
+#:vs|:terminal
+# \tt for terminal in split
+
+# crtl hjkl for resizing
+# leader tk and leader th for swap
+#
+# -----------------------
+# figure out shift tab
+# ctrl k ctrl F
