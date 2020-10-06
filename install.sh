d86197bbe5e6605516ddff3f59873369f8fe3341
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-10-05 21:09:11 -0400

    workon reinstall

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 Sudo apt-get install network-manager xorg xserver-xorg nvidia-detect mesa-utils xutils xinit
+xrdb -merge ~/.Xresources
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/snap/bin:/snap/bin
+sudo snap install nvim
+fc-cache -fv
