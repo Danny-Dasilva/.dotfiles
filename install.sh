b723a1df22372d3fc853e5ccc74eac6abc4ee598
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-10-11 12:21:00 -0400

    work to quick install

--- install.sh
+++ install.sh
@@ -3,3 +3,15 @@ xrdb -merge ~/.Xresources
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/snap/bin:/snap/bin
 sudo snap install nvim
 fc-cache -fv
+sudo apt-get install firmware-iwlwifi
+snapd
+python3-pip
+sudo pip3 install xcffib && sudo pip3 install --no-cache-dir cairocffi
+sudo pip3 install qtile
+git
+sudo apt-get install libpangocairo-1.0.0
+sudo apt-get install libgdk-pixbuf2.0-0
+sudo apt-get install xsel curl
+sudo pip3 install pynvim
+npm
+sudo apt-get install ttf-ancient-fonts
